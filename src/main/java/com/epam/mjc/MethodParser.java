8de6ece092c12ff27ae768093e4cd16d1075952e
Tursyn Sarmassa <tursyn.sarmassa@gmail.com>
2024-04-18 22:23:33 -0400

    s

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,76 @@
 package com.epam.mjc;
 
-public class MethodParser {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
+public class MethodParser {
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        List<MethodSignature.Argument> arguments = new ArrayList<>();
+        MethodSignature methodSignature;
+        String temp[] = signatureString.split("\\(");
+        String splitMethodsData[] = temp[0].split(" ");
+        String parametersData = temp[1].replace(")", "");
+        if(parametersData.length() != 0) {
+            String[] pairParameters = parametersData.split(",");
+            for (int i = 0; i < pairParameters.length; i++) {
+                String pair = pairParameters[i].trim();
+                String[] pair2 = pair.split(" ");
+                MethodSignature.Argument argument = new MethodSignature.Argument(pair2[0], pair2[1]);
+                arguments.add(argument);
+            }
+            if(splitMethodsData.length == 3){
+                methodSignature = new MethodSignature(splitMethodsData[2],arguments);
+                methodSignature.setAccessModifier(splitMethodsData[0]);
+                methodSignature.setReturnType(splitMethodsData[1]);
+            }
+            else{
+                methodSignature = new MethodSignature(splitMethodsData[1], arguments);
+                methodSignature.setReturnType(splitMethodsData[0]);
+            }
+        }
+        else{
+            if (splitMethodsData.length == 3) {
+                methodSignature = new MethodSignature(splitMethodsData[2],arguments);
+                methodSignature.setAccessModifier(splitMethodsData[0]);
+                methodSignature.setReturnType(splitMethodsData[1]);
+            }
+            else{
+                methodSignature = new MethodSignature(splitMethodsData[1], arguments);
+                methodSignature.setReturnType(splitMethodsData[0]);
+            }
+        }
+
+        return methodSignature;
     }
+
+//    public static void main(String[] args) {
+//        String s = "private void log(String value)";
+//        String temp[] = s.split("\\(");
+//        for (int i = 0; i < temp.length; i++) {
+//            System.out.println(temp[i]);
+//        }
+//
+////        StringTokenizer st = new StringTokenizer(s, " ");
+////        while (st.hasMoreTokens())
+////            System.out.println(st.nextToken());
+//    }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,34 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class StringSplitter {
 
     /**
-     * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
+     * Splits given string applying all delimiters to it. Keeps order of result substrings as in source string.
      *
      * @param source source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> substrings = new ArrayList<>();
+        int startIndex = 0;
+        for (int i = 0; i < source.length(); i++) {
+            if (delimiters.contains(String.valueOf(source.charAt(i)))) {
+                // Add substring before delimiter if it's not empty
+                if (startIndex < i) {
+                    substrings.add(source.substring(startIndex, i));
+                }
+                startIndex = i + 1; // Update start index to skip the delimiter
+            }
+        }
+        // Add the last substring if it exists
+        if (startIndex < source.length()) {
+            substrings.add(source.substring(startIndex));
+        }
+        return substrings;
     }
 }
